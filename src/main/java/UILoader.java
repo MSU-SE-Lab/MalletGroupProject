d78556f63e81afd275bca1a74d3b1529240f5ac4
Alex Coté <alx929@gmail.com>
2016-11-09 20:55:23 -0600

    Feature/ui (#21)
    * Created base file for UI using JavaFX.
    
    * Created base file for UI using JavaFX.
    
    * Created base file for UI using JavaFX.
    
    * Created base file for UI using JavaFX.
    
    * Created base file for UI using JavaFX.
    
    * Created base file for UI using JavaFX.
    
    * Created base file for UI using JavaFX.
    
    * Moves UILoader to the main folder
    
    * Added basic files for UI.
    
    * Update to fix GUI loading.

---
+++ src/main/java/UILoader.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UILoader extends Application {
+     @Override
+    public void start(Stage stage) throws Exception {
+         Parent root = FXMLLoader.load(getClass().getResource("jfxTest.fxml"));
+
+         Scene scene = new Scene(root);
+
+         stage.setScene(scene);
+         stage.show();
+     }
+
+     public static void main(String[] args) {
+         launch(args);
+     }
+}
