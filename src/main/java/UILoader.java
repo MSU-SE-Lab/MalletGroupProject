2b77e7d38d4a359c76ac02caa41bdb15f31adece
Alex Coté <alx929@gmail.com>
2016-11-15 10:43:30 -0600

    Added resource files for the main window and results window. Updated UILoader to take some base input that others can use as an example to implement functionality to the UI.

--- src/main/java/UILoader.java
+++ src/main/java/UILoader.java
@@ -1,21 +1,42 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class UILoader extends Application {
      @Override
     public void start(Stage stage) throws Exception {
-         Parent root = FXMLLoader.load(getClass().getResource("jfxTest.fxml"));
+         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
 
          Scene scene = new Scene(root);
 
          stage.setScene(scene);
+         stage.setTitle("Topic Modeler");
          stage.show();
      }
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
          launch(args);
      }
+
+    public void runBtnPressed(ActionEvent actionEvent) throws IOException {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("resultsWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Modeler Results");
+            stage.setScene(new Scene(root));
+            stage.show();
+            // Hide this current window (if this is what you want)
+            //((Node)(ActionEvent.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
