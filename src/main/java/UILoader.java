0430ffb5557487c65f720f31d2b1c7119654d9d9
Jacob Mason <jacob@jacobmason.net>
2016-11-21 17:29:02 -0600

    Cleaned up imports and got rid of test files

--- src/main/java/UILoader.java
+++ src/main/java/UILoader.java
@@ -1,21 +1,8 @@
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
-import javafx.scene.control.Button;
 
 public class UILoader extends Application {
     
